c7bd8b736ceed744cce2f2acfed9f7d3bbd6c03a
0u812 <0u812@users.noreply.github.com>
2018-12-06 07:40:26 -0800

    initl commit

---
+++ make-linux-wheel.sh
@@ -0,0 +1,1 @@
+rm -rf build dist && /opt/python-3.6.5/bin/python3 setup.py bdist_wheel --python-tag=cp34.cp35.cp36 --plat-name=manylinux1-x86_64
---
+++ setup.py
@@ -0,0 +1,20 @@
+#! /usr/bin/env python
+
+from setuptools import setup
+
+setup(name='pysemgenjar-packr',
+      version='0.1.0',
+      description='Jar file and embedded openjdk for pysemgen',
+      author='J. Kyle Medley',
+      packages=['semgenjar'],
+      include_package_data=True,
+      package_data={'semgenjar': [
+          'semgenapi',
+          'SemSimAPI.jar',
+          'jre/*',
+          'jre/**/*',
+      ]},
+      install_requires=[
+        #'tellurium>=2.1.0',
+        ],
+      )
