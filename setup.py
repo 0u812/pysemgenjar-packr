573527821f289af0097549d3666311a70f7fb5a5
0u812 <0u812@users.noreply.github.com>
2018-12-06 07:43:12 -0800

    add cp37

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='pysemgenjar-packr',
-      version='0.1.0',
+      version='0.1.1',
       description='Jar file and embedded openjdk for pysemgen',
       author='J. Kyle Medley',
       packages=['semgenjar'],
